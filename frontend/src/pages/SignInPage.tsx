6e554f929983dfa03c7130a61c16f0843f824dfc
james wilde <jcwilde98@gmail.com>
2026-04-25 07:00:03 -0400

    sign in page skeleton

--- frontend/src/pages/SignInPage.tsx
+++ frontend/src/pages/SignInPage.tsx
@@ -2,6 +2,27 @@ export default function LandingPage() {
     return(
         <div>
             <p>Sign In!</p>
+            <form>
+                <div className='px-4 py-5'>
+                    <label>Username: </label>
+                    <input
+                        className='border'
+                        type='text'
+                        name='username_entry'
+
+                    />
+                </div>
+                <div className='px-4 py-5'>
+                    <label>Password: </label>
+                    <input
+                        className='border'
+                        type='password'
+                        name='password_entry'
+
+                    />
+                </div>
+                <button className='bg-emerald-700 hover:bg-emerald-500 py-4 px-4 text-white mx-auto rounded-sm'>Submit</button>
+            </form>
         </div>
     )
 }
