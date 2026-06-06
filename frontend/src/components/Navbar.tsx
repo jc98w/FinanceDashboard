04fb5da4b99ec556e81e17aa1e34633681f48b9a
James Wilde <jcwilde98@gmail.com>
2026-06-05 21:44:47 -0400

    Navbar responsive to login state

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,13 +1,27 @@
 import { Link } from 'react-router-dom'
+import { useAuth } from '../contexts/AuthContext.tsx'
 
 export default function Navbar() {
+  const { isAuthenticated, logout } = useAuth();
+
   return(
       <nav className="bg-emerald-700 py-4 px-4 text-white mx-auto" >
         <ul className="flex gap-x-5 gap-y-1 flex-wrap">
           <li className="mr-auto"><Link to="/" className="btn-main">Home</Link></li>
-          <li><Link to="/accounts" className="btn-main">Accounts</Link></li>
-          <li><Link to="/register" className="btn-main">Register</Link></li>
-          <li><Link to="login" className="btn-main">Sign In</Link></li>
+
+          { isAuthenticated ? (
+            <>
+              {/* Navbar options when logged in */}
+              <li><Link to="/accounts" className="btn-main">Accounts</Link></li>
+              <li><Link to="/" className="btn-main" onClick={ logout }>Logout</Link></li>
+            </>
+          ) : (
+            <>
+              {/* Navbar options when logged out */}
+              <li><Link to="/register" className="btn-main">Register</Link></li>
+              <li><Link to="login" className="btn-main">Sign In</Link></li>
+            </>
+          )}
         </ul>
       </nav>
   )
