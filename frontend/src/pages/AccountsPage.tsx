7d8e9aaf0372e41c56d2b3d427b03486de52103f
james wilde <jcwilde98@gmail.com>
2026-07-18 16:32:11 -0400

    refactor: Account page buttons style > btn-mini

--- frontend/src/pages/AccountsPage.tsx
+++ frontend/src/pages/AccountsPage.tsx
@@ -58,10 +58,10 @@ export default function AccountsPage() {
             <div className='flex flex-row flex-wrap align-center'>
                 <h1 className='text-xl text-left mr-auto'>ACCOUNTS</h1>
                 <div className='bg-emerald-700 rounded-sm'>
-                    <button className='btn-main text-xs p-2' title='table view' onClick={(event)=>{event.preventDefault(); setViewMode('table')}}><i className='fa fa-table'/></button>
-                    <button className='btn-main text-xs p-2' title='card view' onClick={(event)=>{event.preventDefault(); setViewMode('card')}}><i className='fa fa-arrows-h'/></button>
+                    <button className='btn-mini text-xs w-8' title='table view' onClick={(event)=>{event.preventDefault(); setViewMode('table')}}><i className='fa fa-table'/></button>
+                    <button className='btn-mini text-xs w-8' title='card view' onClick={(event)=>{event.preventDefault(); setViewMode('card')}}><i className='fa fa-arrows-h'/></button>
                 </div>
-                <button className='btn-main text-xs p-2 ml-2' title='add account' onClick={ () => {setIsModalOpen(true) } }><i className='fa fa-plus'/></button>
+                <button className='btn-mini text-xs w-8 ml-2' title='add account' onClick={ () => {setIsModalOpen(true) } }><i className='fa fa-plus'/></button>
             </div>
             <div className='mt-2'>
                 {
