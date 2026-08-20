fca99a9289c277d52f38ba6e0f52d32b10338326
james wilde <jcwilde98@gmail.com>
2026-08-19 23:10:24 -0400

    refactor: account context handles backend account create and delete

--- frontend/src/contexts/AccountContext.tsx
+++ frontend/src/contexts/AccountContext.tsx
@@ -1,4 +1,6 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+import { useAuth } from './AuthContext'
 import type { PropsWithChildren } from 'react';
 import type { Account } from '../types/account';
 
@@ -12,13 +14,34 @@ interface AccountContextType {
 const AccountContext = createContext<AccountContextType | undefined>(undefined);
 
 export const AccountProvider = ({ children }: PropsWithChildren) => {
+    const { isAuthenticated } = useAuth();
     const [ accounts, setAccounts ] = useState<Account[]>([]);
 
     const addAccount = async (newAccount: Account) => {
-        setAccounts((currAccounts) => [...currAccounts, newAccount]);
+        try {
+            const response = await fetch('/api/accounts/create', {
+                method: "POST",
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(newAccount)
+            })
+
+            if (response.ok) {
+                toast.success(`${newAccount.accountName} created`)
+                setAccounts((currAccounts) => [...currAccounts, newAccount]);
+            }
+            else {
+                toast.error('Failed to create account')
+            }
+        }
+        catch {
+            toast.error('Error occurred')
+        }
     }
 
     const updateAccount = async (accountName: string, changes: Partial<Account>) => {
+        // FIXME: update in backend
         setAccounts((currAccounts) => 
             currAccounts.map((account) => {
                 if (account.accountName !== accountName) return account;
@@ -28,8 +51,43 @@ export const AccountProvider = ({ children }: PropsWithChildren) => {
     }
 
     const delAccount = async (accountName: string) => {
+                const response = await fetch('/api/accounts/',
+            {
+                method: "DELETE",
+                headers: {
+                    'Content-type': "application/json"
+                },
+                body: JSON.stringify({ "accountName": accountName })
+            }
+        );
+
+        if (response.ok) {
+            toast.success(`${accountName} deleted`)
+        }
+        else {
+            toast.error(`Something went wrong. Failed to delete ${accountName}`)
+        }
         setAccounts((currAccounts) => currAccounts.filter(account => account.accountName !== accountName));
     }
+
+    // Refresh user's accounts from backend
+    useEffect(() => {
+        if (!isAuthenticated) return;
+
+        const fetchAccounts = async () => {
+            const response = await fetch('/api/accounts/me', {
+                method: "GET",
+            });
+
+            if (!response.ok) {
+                throw new Error('Failed to fetch accounts')
+            }
+            const data = await response.json();
+            setAccounts(data.accounts);
+        }
+
+        fetchAccounts();
+    }, [isAuthenticated])
 
     return (
         <AccountContext.Provider value={{ accounts, addAccount, updateAccount, delAccount}}>
